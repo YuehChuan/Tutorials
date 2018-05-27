91c35a253817ddb6d73d7f4c3759bc841f1e805e
dave <dpynes@gmail.com>
2018-05-26 17:14:29 -0700

    ...

--- LinkedLists/LinkedList_02/main.cpp
+++ LinkedLists/LinkedList_02/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 struct node{
-  int date;
-  string description;
+  int val;
   node* next;
-  node(int dt_, string desc_ ) : date(dt_), description(desc_), next(NULL) {};
+  node(int v_) : val(v_), next(NULL) {};
 };
 
 class linkedlist {
@@ -17,33 +18,26 @@ class linkedlist {
         head = NULL;
      }
 
-     void addSortedNode(int dt_, string desc_ ) {
+     void addSortedNode(int v_) {
        //case 1: the list has not yet started
        if(!head) {
-         head = new node(dt_,desc_);
+         head = new node(v_);
        }
        else{
          node* cur = head;
          node* prev = NULL;
-         node* temp = new node(dt_,desc_);
-         while(cur && dt_ > cur->date) {
-           prev = cur;
-           cur = cur->next;
-         }
-         //case 2: dt_ is the smallest node in the list. Note, this also means the head of the list is now our temp node.
-         if(!prev) {
-           temp->next = head;
-           head = temp;
-         }
-         //case 3: dt_ is in the middle of the list
-         else if(prev && cur){
+         node* temp = new node(v_);
+         while(cur && v_ > cur->val)
+           prev = cur, cur = cur->next;
+         //case 2: v_ is the smallest node in the list.
+         if(!prev)
+           temp->next = head, head = temp;
+         //case 3: v_ is in the middle of the list
+         else if(prev && cur)
+           prev->next = temp, temp->next = cur;
+         //case 4: v_ is at the end of the list
+         else if(!cur)
            prev->next = temp;
-           temp->next = cur;
-         }
-         //case 4: dt_ is at the end of the list
-         else if(!cur) {
-           prev->next = temp;
-         }
        }
      }
 
@@ -51,27 +45,25 @@ class linkedlist {
        node* temp = head;
        cout<<"Printing list: "<<endl<<"[";
        while(temp->next) {
-         cout<<"{date: "<<temp->date<<", description: \""<<temp->description<<"\"}, "<<endl;
+         cout<<temp->val<<", ";
          temp = temp->next;
        }
        if(temp)
-         cout<<"{date: "<<temp->date<<", description: \""<<temp->description<<"\"}";
+         cout<<temp->val;
        cout<<"]"<<endl;
      }
 };
 
+/*
+//bonus code to be used in merging linked lists//
 node* mergeLists(linkedlist a, linkedlist b) {
   node* temp_a = a.head;
   node* temp_b = b.head;
   node* runner;
-  if( temp_a->date <= temp_b->date ) {
-    runner = temp_a;
-    temp_a = temp_a->next;
-  }
-  else {
-    runner = temp_b;
-    temp_b = temp_b->next;
-  }
+  if( temp_a->val <= temp_b->val )
+    runner = temp_a, temp_a = temp_a->next;
+  else
+    runner = temp_b, temp_b = temp_b->next;
   node* head = runner;
 
   while(runner) {
@@ -83,35 +75,21 @@ node* mergeLists(linkedlist a, linkedlist b) {
       runner->next = temp_a;
       break;
     }
-    if(temp_a->date <= temp_b->date) {
-      runner->next = temp_a;
-      temp_a = temp_a->next;
-    }
-    else {
-      runner->next = temp_b;
-      temp_b = temp_b->next;
-    }
+    if(temp_a->val <= temp_b->val)
+      runner->next = temp_a, temp_a = temp_a->next;
+    else
+      runner->next = temp_b, temp_b = temp_b->next;
     runner = runner->next;
   }
   return head;
 }
+*/
 
 int main() {
-  linkedlist ll_a;
-  ll_a.addSortedNode(5,"I");
-  ll_a.addSortedNode(4,"harder");
-  ll_a.addSortedNode(6,"work");
-  ll_a.addSortedNode(8,"luckier");
-  ll_a.addSortedNode(7,"the");
-
-  linkedlist ll_b;
-  ll_b.addSortedNode(3,"The");
-  ll_b.addSortedNode(10,"get");
-  ll_b.addSortedNode(9,"I");
-
-  ll_a.head = mergeLists(ll_a,ll_b);
-
-  ll_a.print();
-
+  srand(time(NULL));
+  linkedlist ll;
+  for(int i=0; i<15; i++)
+    ll.addSortedNode(rand()%100);
+  ll.print();
   return 0;
 }
