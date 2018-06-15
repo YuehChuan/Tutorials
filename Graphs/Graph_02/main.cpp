1d6e9637b9a8d955621c6de5415e51e739fd7d97
dave <dpynes@gmail.com>
2018-06-14 17:37:57 -0700

    ...

--- Graphs/Graph_02/main.cpp
+++ Graphs/Graph_02/main.cpp
@@ -4,6 +4,7 @@
 #include <time.h>    //make the random number random
 #include <math.h>    //power function
 #include <limits.h>  //max integers
+#include <vector>    //to store the path(s)
 
 //define size of maze//
 #define HEIGHT 25
@@ -28,8 +29,17 @@ float euclideanDistance(point a, point b) {
   return (pow( pow( a.x - b.x, 2.0) + pow( a.y - b.y, 2.0), 0.5 ));
 }
 
-//Define print, templated for int and float//
-void print(int arr[HEIGHT][WIDTH]) {
+template<class T>
+void print(T arr[HEIGHT][WIDTH]) {
+  for(int i=0; i<HEIGHT; i++) {
+    for(int j=0; j<WIDTH; j++)
+      std::cout<<arr[i][j];
+    std::cout<<"\n";
+  }
+}
+
+void print(int arr[HEIGHT][WIDTH], std::vector<point> path) {
+  for(point p : path) { arr[p.y][p.x] = -1; }
   for(int i=0; i<HEIGHT+2; i++) {
     for(int j=0; j<WIDTH+2; j++) {
       if(!arr[i-1][j-1] || j == 0 || (j == WIDTH+1) || (i == 0) || (i == HEIGHT+1) )
@@ -45,8 +55,9 @@ void print(int arr[HEIGHT][WIDTH]) {
   }
 }
 
+
 /* Generate a maze using a depth first search and recursive stack.
-   Randomly select a point's neighbor (L,R,U,D). Check boundary
+   Randomly select a point's neighbor (L,R,U,D), check boundary
    conditions At that point and check if there is a wall. If
    there is a wall convert that wall to a path. Note, move in increments of two.
 */
@@ -80,11 +91,15 @@ void randomMaze(int maze[HEIGHT][WIDTH], point p) {
 
 /* A* is a variation of Dijkstras algorithm.
    full explanation: http://www..... */
-void astar(int maze[HEIGHT][WIDTH], point s, point g) {
+std::vector<point> astar(int maze[HEIGHT][WIDTH], point s, point g) {
   //define sets for distance and visited, and initialize sets//
+  std::vector<point> paths[HEIGHT][WIDTH];
   float dist[HEIGHT][WIDTH] = { 0 };
   bool visited[HEIGHT][WIDTH] = { 0 };
-  for(int i=0; i<HEIGHT*WIDTH; i++) { dist[(int)floor(i/HEIGHT)][(i%WIDTH)] = INT_MAX; }
+
+  for(int i=0; i<HEIGHT; i++)
+    for(int j=0; j<WIDTH; j++)
+      dist[i][j] = INT_MAX;
 
   //set current to starting point//
   point cur = s;
@@ -95,19 +110,21 @@ void astar(int maze[HEIGHT][WIDTH], point s, point g) {
     //update visited at current//
     visited[cur.y][cur.x] = 1;
 
-    //neighbors//
+    //neighboring points//
     point nb[4] = {
-      point(cur.x-1,cur.y),
-      point(cur.x+1,cur.y),
-      point(cur.x,cur.y-1),
-      point(cur.x,cur.y+1)
+      point(cur.x-1,cur.y,direction::L),
+      point(cur.x+1,cur.y,direction::R),
+      point(cur.x,cur.y-1,direction::U),
+      point(cur.x,cur.y+1,direction::D)
     };
 
-    //calculate distance at neighbors and possibly update.//
+    //calculate distance at neighbors and update distance if calculated distance is smaller//
     for(point cn : nb )
       if( cn.inBounds() && maze[cn.y][cn.x] &&
-        (euclideanDistance(cn,g) + dist[cur.y][cur.x] + maze[cn.y][cn.x] < dist[cn.y][cn.x]) )
+        (euclideanDistance(cn,g) + dist[cur.y][cur.x] + maze[cn.y][cn.x] < dist[cn.y][cn.x]) ) {
           dist[cn.y][cn.x] = euclideanDistance(cn,g) + dist[cur.y][cur.x] + maze[cn.y][cn.x];
+          paths[cn.y][cn.x] = paths[cur.y][cur.x], paths[cn.y][cn.x].push_back(cur);
+      }
 
     //select the next point to be current//
     cur = point(-1,-1);
@@ -118,43 +135,36 @@ void astar(int maze[HEIGHT][WIDTH], point s, point g) {
           cur = point(j,i), md = dist[i][j];
   }
 
-  for(int i=0; i<HEIGHT*WIDTH; i++) { visited[(int)floor(i/HEIGHT)][(i%WIDTH)] = 0; }
-  while( !(s == cur) ) {
-    visited[cur.y][cur.x] = 1;
-    maze[cur.y][cur.x] = -1;
-    point nb[4] = {
-      point(cur.x-1,cur.y),
-      point(cur.x+1,cur.y),
-      point(cur.x,cur.y-1),
-      point(cur.x,cur.y+1)
-    };
-    float md = INT_MAX;
-    for(point cn : nb) {
-      if( cn.inBounds() && dist[cn.y][cn.x] < md && !visited[cn.y][cn.x] )
-        md = dist[cn.y][cn.x], cur = cn;
-    }
-  }
-  maze[cur.y][cur.x] = -1;
-  print(maze);
+  //push the goal point to the path, and return the path from start to goal//
+  paths[g.y][g.x].push_back(g);
+  return paths[g.y][g.x];
+
 }
 
 int main() {
+
+  //seed random number generator//
   srand(time(NULL));
-  //generate a maze//
+
+  //create random maze//
   int maze[HEIGHT][WIDTH] = { 0 };
   randomMaze(maze, point(rand()%WIDTH,rand()%HEIGHT) );
-  //generate a start and goal point. Points must be on the path//
-  point s,g;
-  while( !(s.inBounds() && g.inBounds()) || !(maze[s.y][s.x] && maze[g.y][g.x])  ) {
-    s = point(rand()%HEIGHT,rand()%WIDTH), g = point(rand()%HEIGHT,rand()%WIDTH);
-  }
 
+  //select random points//
+  point s, g;
+  while( !(s.inBounds() && g.inBounds()) || !(maze[s.y][s.x] && maze[g.y][g.x])  )
+    s = point(rand()%HEIGHT,rand()%WIDTH), g = point(rand()%HEIGHT,rand()%WIDTH);
+
+  //solve with a* algorithm//
+  std::vector<point> path = astar(maze,s,g);
+
+  //output information to console//
   std::cout<<"\nStarting from: ";
   s.print();
   std::cout<<"Going to: ";
   g.print();
   std::cout<<"\n";
-  astar(maze,s,g);
-  std::cout<<"\n";
+  print(maze,path);
+
   return 0;
 }
